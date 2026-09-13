0562378c2027f6c3ab784db2923f99c92e058e84
Debora Peter Hello <d.hello1@alustudent.com>
2026-09-13 08:00:37 +0400

    FHC initial commit

---
+++ file_handling.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+
+class Filehandling:
+    """ This class uses pandas to save and load the system's csv files"""
+
+    def __init__(self):
+        pass
+
+    def prepare_directory(self):
+        pass
+
+    def save_changes(self):
+        pass
+
+    def load_products(self):
+        pass
+
+    def save_products(self):
+        pass
+
+
+    def load_sales(self):        
+        pass
+
+    def save_sales(self):
+        pass
+
+    def load_income(self):
+        pass
+
+
+    def save_income(self):
+        pass
