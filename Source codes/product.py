b5a3fee6e7f525e4b088fa7a29a7964542b9593a
[oinusah] <[oinusah@alustudent.com]>
2026-09-16 13:10:25 +0400

    Add compiled Python by""tecode for product, sales, and user interface modules
    - Generated bytecode files for product.py, sales.py, and user_interface.py.
    - These files are located in the __pycache__ directory and are optimized for Python 3.13.
    - This commit improves the performance of the application by utilizing precompiled bytecode.

--- Source codes/product.py
+++ Source codes/product.py
@@ -17,7 +17,7 @@ class Product:
         self.size=size._clean_optional_text(size)
         self.supplier=supplier._clean_optional_text(supplier)
         self.price=self._validate_price(price)
-        self.quantity=self._validate_quantity(quantity)
+        self.quantity=self.validate_quantity(quantity)
         self.entry_date=(self._validate_date(entry_date) if entry_date else datetime.now().strftime(self.DATE_FORMAT))
         self.expiry_date=self._validate_date(expiry_date) if expiry_date else None
 
